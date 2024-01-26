c6290fb25f9951ce8f8eb76d1dc3e07d19bc47a2
Huseyn Sharifzade <huseynsharif@gmail.com>
2024-01-26 14:37:27 +0400

    First commit

---
+++ src/main/java/com/ict/autoemailapp/AutoEmailAppApplication.java
@@ -0,0 +1,13 @@
+package com.ict.autoemailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoEmailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoEmailAppApplication.class, args);
+	}
+
+}
